b4c39f1c2d1607d31d93913b8f338401ce5b6940
Ashish Singh <ashishsinghrana39@gmail.com>
2025-03-06 16:53:04 +0530

    UC3

--- src/main/java/com/example/EmployeePayroll/repository/EmployeeRepo.java
+++ src/main/java/com/example/EmployeePayroll/repository/EmployeeRepo.java
@@ -1,5 +1,6 @@
 package com.example.EmployeePayroll.repository;
 
+import com.example.EmployeePayroll.dto.EmployeeDTO;
 import com.example.EmployeePayroll.model.EmployeeModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
